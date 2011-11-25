478d168ee3837659c25c2f73053b324e5f4d1250
Micah Breedlove <druid628@gmail.com>
2011-11-25 16:58:32 -0600

    modified route and setup bsdApi/get to accept predefined named queries

--- modules/bsdApi/actions/action.class.php
+++ modules/bsdApi/actions/action.class.php
@@ -12,7 +12,29 @@ class bsdApiActions extends PluginBsdApiActions
 
    public function executeGet(sfWebRequest $request)
    {
+       $appClass = sprintf("%s", ucfirst($request->getParameter('appClass')));
+       if($request->hasParameter('query'))
+       {
+          $appClassTable == Doctrine::getTable($appClass);
+          $query = sprintf("%s", $request->getParameter('query'));
+          $result = $appClassTable->$query();
+          return json_encode($result);
+       }
+
+       $query = Doctrine_Query::create()
+                ->from($appClass . " ac");
+                /**
+                 * need to think of the best
+                 * way to approach this
+                 * because i can think of a few
+                 * but i'm in favor of doing 
+                 * some sort of array loop
+                 * or a ->select(join("," $array);
+                 * think about it and we'll 
+                 * come back to it
+                 */
    }
+
 
    public function executeGive(sfWebRequest $request)
    {
