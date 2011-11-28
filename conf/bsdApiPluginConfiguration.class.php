3f8f90499140ebd0d85483cb799cd28ccf9366d0
Micah Breedlove <druid628@gmail.com>
2011-11-27 21:44:23 -0600

    modified sfGuardSecurity for each module and a few minor changes

--- conf/bsdApiPluginConfiguration.class.php
+++ conf/bsdApiPluginConfiguration.class.php
@@ -23,49 +23,7 @@ class bsdApiPluginConfiguration extends sfPluginConfiguration
    */
   public function initialize()
   {
-
-	$this->dispatcher->connect('routing.load_configuration', array('bsdApiRouting', 'listenToRoutingLoadConfigurationEvent'));
-	#$this->dispatcher->connect('user.method_not_found', array('bsdApiUser', 'methodNotFound'));
-
-  }
-  public function listenToRoutingLoadConfiguration(sfEvent $event)
-  {
-      // $routing = $event->getSubject();
-      // if($sfConfig::get())
-  $r = sfRouting::getInstance();
-  $routes = $r->getRoutes();
-  $r->clearRoutes();
-
-  // Plugin home
-  $r->connect('plugin_home', '/my_super_plugin/homepage', array(
-    'module' => 'my_plugin_module',
-    'action' => 'my_plugin_action',
-    'additional_parameter'   => 1
-    ));
-
-  // Another route
-  $r->connect('plugin_home', '/my_super_plugin/section1', array(
-    'module' => 'my_plugin_module',
-    'action' => 'my_plugin_action_section1',
-    'additional_parameter'   => 2
-    ));
-
-  // ... other routes
-
-  // Then merge new routes with the saved one
-  $r->setRoutes($r->getRoutes() + $routes);
-  }
- /*   if (sfConfig::get('app_sf_guard_plugin_routes_register', true) && in_array('sfGuardAuth', sfConfig::get('sf_enabled_modules', array())))
-    {
-      $this->dispatcher->connect('routing.load_configuration', array('sfGuardRouting', 'listenToRoutingLoadConfigurationEvent'));
-    }
-
-    foreach (array('sfGuardUser', 'sfGuardGroup', 'sfGuardPermission') as $module)
-    {
-      if (in_array($module, sfConfig::get('sf_enabled_modules', array())))
-      {
-        $this->dispatcher->connect('routing.load_configuration', array('sfGuardRouting', 'addRouteForAdmin'.str_replace('sfGuard', '', $module)));
-      }
-    } */
+    	$this->dispatcher->connect('routing.load_configuration', array('bsdApiRouting', 'listenToRoutingLoadConfigurationEvent'));
+	    #$this->dispatcher->connect('user.method_not_found', array('bsdApiUser', 'methodNotFound'));
   }
 }
