2b07ae9bdb914775d9ed4f473fe34badd35dc659
Micah Breedlove <micah.breedlove@iostudio.com>
2011-12-01 08:13:45 -0600

    Code formatting removed some old debug code

--- lib/routing/bsdApiRouting.class.php
+++ lib/routing/bsdApiRouting.class.php
@@ -1,30 +1,31 @@
 <?PHP
 
-class bsdApiRouting {
+class bsdApiRouting
+{
 
-	public static function listenToRoutingLoadConfigurationEvent(sfEvent $event) {
+	public static function listenToRoutingLoadConfigurationEvent(sfEvent $event)
+	{
 		$routing = $event->getSubject();
 
 
-		if (in_array('bsdApi', sfConfig::get('sf_enabled_modules')) )
+		if (in_array('bsdApi', sfConfig::get('sf_enabled_modules')))
 		{
-		    $routing->prependRoute('api_deauth', new sfRoute(
-        		'/api/revoke/:token',
-        		array('module' => 'bsdApi', 'action' => 'revoke') ));
-		    $routing->prependRoute('api_get', new sfRoute(
-        		'/api/get/:token/:appClass',
-        		array('module' => 'bsdApi', 'action' => 'get') ));
-		    $routing->prependRoute('api_give', new sfRoute(
-        		'/api/give/:token/:isNew/:appClass',
-        		array('module' => 'bsdApi', 'action' => 'give') ));
-
+			$routing->prependRoute('api_deauth', new sfRoute(
+					'/api/revoke/:token',
+					array('module' => 'bsdApi', 'action' => 'revoke')));
+			$routing->prependRoute('api_get', new sfRoute(
+					'/api/get/:token/:appClass',
+					array('module' => 'bsdApi', 'action' => 'get')));
+			$routing->prependRoute('api_give', new sfRoute(
+					'/api/give/:token/:isNew/:appClass',
+					array('module' => 'bsdApi', 'action' => 'give')));
 		}
-		if(in_array('bsdApiControl', sfConfig::get('sf_enabled_modules')))
+		
+		if (in_array('bsdApiControl', sfConfig::get('sf_enabled_modules')))
 		{
-		    $routing->prependRoute('api_learn', new sfRoute(
-        		'/api/learn_me',
-        		array('module' => 'bsdApiControl', 'action' => 'learnMe') ));
-
+			$routing->prependRoute('api_learn', new sfRoute(
+					'/api/learn_me',
+					array('module' => 'bsdApiControl', 'action' => 'learnMe')));
 		}
 	}
 
--- modules/bsdApi/actions/actions.class.php
+++ modules/bsdApi/actions/actions.class.php
@@ -9,92 +9,87 @@
  */
 class bsdApiActions extends PluginBsdApiActions
 {
-/*
-REFERENCE - because I can't ever keep this straight
-Doctrine_Record::STATE_DIRTY = 1
-Doctrine_Record::STATE_TDIRTY = 2
-Doctrine_Record::STATE_CLEAN = 3
-Doctrine_Record::STATE_TCLEAN = 5
---
-Doctrine_Record::STATE_PROXY = 4
-Doctrine_Record::STATE_LOCKED = 6
-*/
+	/*
+	  REFERENCE - because I can't ever keep this straight
+	  Doctrine_Record::STATE_DIRTY = 1
+	  Doctrine_Record::STATE_TDIRTY = 2
+	  Doctrine_Record::STATE_CLEAN = 3
+	  Doctrine_Record::STATE_TCLEAN = 5
+	  --
+	  Doctrine_Record::STATE_PROXY = 4
+	  Doctrine_Record::STATE_LOCKED = 6
+	 */
 
-   public function executeGet(sfWebRequest $request)
-   {
-       $appClass = sprintf("%s", ucfirst($request->getParameter('appClass')));
-       if($request->hasParameter('query'))
-       {
-          $appClassTable = Doctrine::getTable($appClass);
-          $query = sprintf("%s", $request->getParameter('query'));
-	        $result = $appClassTable->createNamedQuery($query)->setHydrationMode(Doctrine::HYDRATE_ARRAY);
-	        // $result = $appClassTable->createNamedQuery($query)->setHydrationMode(Doctrine::HYDRATE_ARRAY)->execute();
-          // $this->apiResult = json_encode($result);
+	public function executeGet(sfWebRequest $request)
+	{
+		$appClass = sprintf("%s", ucfirst($request->getParameter('appClass')));
+		if ($request->hasParameter('query'))
+		{
+			$appClassTable = Doctrine::getTable($appClass);
+			$query = sprintf("%s", $request->getParameter('query'));
+			$result = $appClassTable->createNamedQuery($query)->setHydrationMode(Doctrine::HYDRATE_ARRAY);
+		} else
+		{
 
-       }
-       else
-       {
+			$result = Doctrine_Query::create()
+				->from($appClass . " ac");
 
-           $result = Doctrine_Query::create()
-               ->from($appClass . " ac");
-               
-                /**
-                 * need to think of the best
-                 * way to approach this
-                 * because i can think of a few
-                 * but i'm in favor of doing 
-                 * some sort of array loop
-                 * or a ->select(join("," $array);
-                 * think about it and we'll 
-                 * come back to it
-                 */
-           $result->setHydrationMode(Doctrine::HYDRATE_ARRAY);
-       } // else
-       $this->apiResult = json_encode($result->execute());
-   } // get
+			/**
+			 * need to think of the best
+			 * way to approach this
+			 * because i can think of a few
+			 * but i'm in favor of doing 
+			 * some sort of array loop
+			 * or a ->select(join("," $array);
+			 * think about it and we'll 
+			 * come back to it
+			 */
+			$result->setHydrationMode(Doctrine::HYDRATE_ARRAY);
+		} // else
+		$this->apiResult = json_encode($result->execute());
+	} // get
 
 
-   public function executeGive(sfWebRequest $request)
-   {
-       $requestParams2Ignore = array('module', "action", "token", "isNew", "appClass");
-       $appClass = sprintf("%s", ucfirst($request->getParameter('appClass')));
+	public function executeGive(sfWebRequest $request)
+	{
+		$requestParams2Ignore = array('module', "action", "token", "isNew", "appClass");
+		$appClass = sprintf("%s", ucfirst($request->getParameter('appClass')));
 
-       if($request->hasParameter('isNew') && $request->getParameter('isNew') == true)
-       {
-	       $it = new $appClass;
-         
-         $stuffFromRequest = $request->getParameterHolder()->getAll();
-         
-         /**
-          * remove module action token isNew and appClass
-          */
-         foreach($requestParams2Ignore as $unNeeded)
-         {
-            unset($stuffFromRequest[$unNeeded]);
-         }
+		if ($request->hasParameter('isNew') && $request->getParameter('isNew') == true)
+		{
+			$it = new $appClass;
+			$stuffFromRequest = $request->getParameterHolder()->getAll();
 
-	       foreach($stuffFromRequest as $attrb => $parm)
-	       {
-			     $it->set($attrb, $parm);
-	       }
-         $it->save();
+			/**
+			 * remove module action token isNew and appClass
+			 */
+			foreach ($requestParams2Ignore as $unNeeded)
+			{
+				unset($stuffFromRequest[$unNeeded]);
+			}
 
-	       if($it->state() == Doctrine_Record::STATE_CLEAN)
-          		$this->apiResult = json_encode(array("success" => "true"));
-         else
-              $this->apiResult = json_encode(array("success" => "false"));
+			foreach ($stuffFromRequest as $attrb => $parm)
+			{
+				$it->set($attrb, $parm);
+			}
 
-       }
+			$it->save();
 
-   }
+			if ($it->state() == Doctrine_Record::STATE_CLEAN)
+				$this->apiResult = json_encode(array("success" => "true"));
+			else
+				$this->apiResult = json_encode(array("success" => "false"));
+		}
+	}
 
-   public function executeRevoke(sfWebRequest $request)
-   {
-      $this->apiUser()->setApiAccess(false);
-      $this->apiUser()->save();
-      if($this->apiUser()->state() == Doctrine::STATE_CLEAN)
-          $this->apiResult = json_encode(array("success" => "true"));
-      else
-          $this->apiResult = json_encode(array("success" => "false"));
-   }
+	public function executeRevoke(sfWebRequest $request)
+	{
+		$this->apiUser()->setApiAccess(false);
+		$this->apiUser()->save();
+		if ($this->apiUser()->state() == Doctrine::STATE_CLEAN)
+			$this->apiResult = json_encode(array("success" => "true"));
+		else
+			$this->apiResult = json_encode(array("success" => "false"));
+	}
+
 }
