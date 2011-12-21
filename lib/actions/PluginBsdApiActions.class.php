bfa576e20b59e1fb0e4a4a52419ec6a42d3a6ffc
Micah Breedlove <druid628@gmail.com>
2011-12-20 23:21:21 -0600

    Added force ajax option

--- lib/actions/PluginBsdApiActions.class.php
+++ lib/actions/PluginBsdApiActions.class.php
@@ -1,16 +1,27 @@
 <?PHP
 
+
 /**
- * PluginBsdApi (base) actions
- *
- * @package    bsdApiPlugin
- * @author     druid628
+ * PluginBsdApiActions (base)
+ * 
+ * @uses sfActions
+ * @package 
+ * @version $id$
+ * @copyright 2010-2011 Blue Shamrock Development
+ * @author Micah Breedlove <micah@blueshamrock.com> 
+ * @license BSD Version 3.0 {@link https://raw.github.com/blueshamrockdev/bsdApiPlugin/940ae1645ea362bada34a1025730d85853060f80/LICENSE}
  */
 class PluginBsdApiActions extends sfActions
 {
 	protected $apiUser;
 	protected $_token;
 
+  /**
+   * authUser 
+   * 
+   * @access public
+   * @return void
+   */
 	public function authUser()
 	{
 		if ($this->apiUser = Doctrine::getTable('bsdApiUser')->findOneByApiKey($this->_token))
@@ -21,8 +32,19 @@ class PluginBsdApiActions extends sfActions
 		return false;
 	}
 
+  /**
+   * execute 
+   * 
+   * @param sfWebRequest $request 
+   * @access public
+   * @return void
+   */
 	public function execute($request)
 	{
+
+    if(sfConfig::get('app_bsdapi_ajax') == true)
+      $this->forward404Unless($request->isXmlHttpRequest(), "Requires AJAX Request");
+
 		if (!$request->hasParameter('token'))
 			throw new sfCommandArgumentsException(sprintf('bsdApiAction api token was not found. Authorization epic failed!'));
 
@@ -37,5 +59,20 @@ class PluginBsdApiActions extends sfActions
 		$this->setLayout(false);
 		parent::execute($request);
 	}
+
+  /**
+   * renderJSON 
+   * 
+   * @param array $array 
+   * @access protected
+   * @return void
+   */
+  protected function renderJSON(array $array)
+  {
+
+    $this->getResponse()->setHttpHeader('Content-Type', 'text/plain');
+    return $this->renderText(json_encode($arr));
+    
+  }
 
 }
